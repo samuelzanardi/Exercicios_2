f7185aa588ca783dc61fc58a6f100fbb03e54f16
unknown <samuelmoreira.dev@gmail.com>
2022-05-11 14:27:25 -0300

    Começo

---
+++ 01.js
@@ -0,0 +1,6 @@
+/*Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+função) e com ponto de exclamação "!" no final.*/
+
+const cumprimentar = nome => console.log(`Olá ${nome} !`)
+
+cumprimentar("Samuel")
